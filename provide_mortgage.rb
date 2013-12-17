83e264338aba5b3c67ec89b3ae1ccdf0ba75053e
wevans <wevans@cix.co.uk>
2013-12-17 15:30:11 +0000

    After first two challenges Ruby 3

--- provide_mortgage.rb
+++ provide_mortgage.rb
@@ -1,15 +1,16 @@
 def provide_mortgage?(salary, deposit, property_value)
 	loan_amount = property_value - deposit
-	min_deposit = 0.2 # 20%
+
+	min_deposit = property_value < 650000 ? 0.05 : 0.2 # 5% if property is less than $650000 and 20% if it is $650000 or more
 	max_multiplier = 5 # Maximum multiple of annual income that can be borrowed
-	deposit >= property_value * min_deposit && salary * max_multiplier >= loan_amount
+	deposit >= property_value * min_deposit && (salary * max_multiplier >= loan_amount || deposit >= property_value * 0.75)
 end
 
 
 
 
-my_salary = 45000
-my_deposit = 10000
-my_property_value = 250000
+my_salary = 5000
+my_deposit = 140000
+my_property_value = 200000
 
 puts "I can have a mortgage: #{provide_mortgage?(my_salary, my_deposit, my_property_value)}"
